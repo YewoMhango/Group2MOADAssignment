7013c536ff4a51f0790f10eb4cba14660e74d621
YewoMhango <mhangoyewoh@gmail.com>
2022-02-10 15:49:59 +0200

    Initial commit

---
+++ app/src/main/java/com/example/group2moadassignment/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.group2moadassignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
